fbe7d0cdb918f5d24db2c80ac83f323a44457e1a
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2019-06-24 13:50:53 +0200

    Further improved build scripts (use active Conda environment).

--- Release_sophiaAnnotate/build-sophiaAnnotate.sh
+++ Release_sophiaAnnotate/build-sophiaAnnotate.sh
@@ -3,17 +3,17 @@
 set -uex
 trap 'echo "Compilation failed with an error" >> /dev/stderr' ERR
 
+CONDA_PREFIX="${CONDA_PREFIX:?No CONDA_PREFIX -- no active Conda environment}"
+
 install_strtk() {
     wget -c https://github.com/ArashPartow/strtk/raw/master/strtk.hpp -O ../include/strtk.hpp
-    # sed -ir 's/#include <boost\/lexical_cast.hpp>/#include <boost\/convert\/lexical_cast.hpp>/' ../include/strtk.hpp
 }
 
 install_strtk
 
 CPP=x86_64-conda_cos6-linux-gnu-g++
-CONDA_ENV_ROOT=/data/kensche/work/share/miniconda3/envs/tools
-INCLUDES="-I../include -I$CONDA_ENV_ROOT/include"
-CPP_OPTS="-L$CONDA_ENV_ROOT/lib -std=c++1z $INCLUDES -O3 -Wall -Wextra -static -static-libgcc -static-libstdc++ -flto -c -fmessage-length=0 -Wno-attributes"
+INCLUDES="-I../include -I$CONDA_PREFIX/include"
+CPP_OPTS="-L$CONDA_PREFIX/lib -std=c++1z $INCLUDES -O3 -Wall -Wextra -static -static-libgcc -static-libstdc++ -flto -c -fmessage-length=0 -Wno-attributes"
 
 if [[ "${STATIC:-false}" == "true" ]]; then
     CPP_OPTS="-static -static-libgcc -static-libstdc++ $CPP_OPTS"
@@ -33,4 +33,4 @@ $CPP $CPP_OPTS -o "SuppAlignmentAnno.o" "../src/SuppAlignmentAnno.cpp"
 $CPP $CPP_OPTS -o "SvEvent.o" "../src/SvEvent.cpp"
 $CPP $CPP_OPTS -o "sophiaAnnotate.o" "../sophiaAnnotate.cpp"
 
-$CPP -L$CONDA_ENV_ROOT/lib -flto -o "sophiaAnnotate"  AnnotationProcessor.o Breakpoint.o BreakpointReduced.o ChrConverter.o DeFuzzier.o GermlineMatch.o MrefEntry.o MrefEntryAnno.o MrefMatch.o SuppAlignment.o SuppAlignmentAnno.o SvEvent.o  sophiaAnnotate.o   -lz -lboost_system -lboost_iostreams
+$CPP -L$CONDA_PREFIX/lib -flto -o "sophiaAnnotate"  AnnotationProcessor.o Breakpoint.o BreakpointReduced.o ChrConverter.o DeFuzzier.o GermlineMatch.o MrefEntry.o MrefEntryAnno.o MrefMatch.o SuppAlignment.o SuppAlignmentAnno.o SvEvent.o  sophiaAnnotate.o   -lz -lboost_system -lboost_iostreams
