2cb2036e4bd5eaa5847d35ca47ae8043f05ce536
Philip R. Kensche <p.kensche@dkfz-heidelberg.de>
2019-11-18 10:07:32 +0100

    Fixed affiliation.

--- include/HelperFunctions.h
+++ include/HelperFunctions.h
@@ -2,7 +2,7 @@
  * HelperFunctions.h
  *
  *  Created on: 23 May 2019
- *      Author: Philip R. Kensche, DKFZ Heidelberg (Divisions of Theoretical Bioinformatics, Bioinformatics and Omics Data Analytics and currently Neuroblastoma Genomics)
+ *      Author: Philip R. Kensche, DKFZ Heidelberg (Omics IT and Data Management Core Facility)
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
